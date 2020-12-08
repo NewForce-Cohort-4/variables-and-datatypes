6eae6a59f8d0b28ffab1700f6637e2ad8721f068
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-12-08 17:53:51 -0500

    finished live code from saturday

---
+++ script.js
@@ -0,0 +1,46 @@
+//alert("Hello, world")
+
+// console.log("howdy, world!!!!!!!!!!!!!!!!!!");
+
+// let myName = "Jordan"; // declaring and assigning
+
+// console.log(myName);
+
+// myName = "Penelope"; // reassigning
+
+// console.log(myName);
+
+// let myDogsName; // declaration
+
+// myDogsName = "Hoagie"; // assignment
+
+// let bioSentence = `My name is ${myName} and my dog's name is ${myDogsName}` // string interpolation
+// console.log(bioSentence)
+
+// // Numbers
+
+
+// let myDogsAge = 4;
+
+// let dogYearsPerYear = 7;
+
+// let myDogsAgeInHumanYears = myDogsAge*dogYearsPerYear;
+
+// let sum = "2" + "2"
+
+// // Boolean
+// let firstDayIsAlmostOver = true;
+
+
+
+// firstName should hold a string of your first name
+let firstName = "Jordan"
+// lastName should hold a string of your last name
+let lastName = "Castelloe"
+// age should hold a number of your age
+let age = 29
+// likesTravel should hold a boolean representing whether or not you like to travel
+let likesTravel = false
+// Log each variable to the console.
+console.log(firstName, lastName, age, likesTravel)
+
