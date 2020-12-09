8d3697acd95c02634528ee4adde8aed174d5f4af
Jordan Castelloe <jordan.castelloe@gmail.com>
2020-12-08 19:40:11 -0500

    finished example code for arrays and objects

--- script.js
+++ script.js
@@ -44,3 +44,61 @@ let likesTravel = false
 // Log each variable to the console.
 console.log(firstName, lastName, age, likesTravel)
 
+
+let nameOne = "Vincent"
+
+let nameTwo = "Gabriel"
+
+let nameThree = "Aaron"
+
+let cohortFour = ["Vincent", "Gabriel", "Aaron", "Patti", "Mandy", 10000, true]
+console.log(cohortFour)
+
+console.log(cohortFour[2]) // this should be aaron
+
+console.log(cohortFour[4]) // this should be mandy
+
+console.log(cohortFour.length) // should be 7
+
+let favoriteMovies = ["Sandlot", "Matilda", "Ghost Busters"]
+
+console.log(favoriteMovies[0]) // should be sandlot
+
+console.log(favoriteMovies[1]) // should be matilda
+
+console.log(favoriteMovies[2]) // should be ghost busters
+
+
+// let sandlot = ["Sandlot", "Herbert Hoover", 1972]
+
+let sandlot = {
+    title: "Sandlot", // key value pair
+    director: {
+        name: "Herbert Hoover",
+        yearBorn: 1973
+    },
+    yearMade: 1993,
+    cast: ["Tom Geary", "Mike Vicauoeirueoirualdsfjoeiwrueoiru", "Davis Leary"],
+}
+
+console.log(sandlot)
+
+console.log(sandlot.title) // should be "sandlot"
+
+// sandlot.director = "David Mickey Evans"
+
+console.log(sandlot.cast[2])
+
+console.log(sandlot.director.yearBorn)
+
+let jordanObject = {
+   firstName: "Jordan",
+   lastName: "Castelloe",
+   age: 29,
+   likesToTravel: false,
+   favoriteMovies: favoriteMovies
+}
+
+
+
+
